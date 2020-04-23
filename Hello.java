a9c5e22de34682eb35bcd1ebbc41968454885ef8
kim-sun-ho <64174490+kim-sun-ho@users.noreply.github.com>
2020-04-23 09:04:48 +0900

    Hello.java 생성

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class  ksh{
+ 
+    public static void main(String[] args) {
+        for(int i=1; i<=9; i++) {
+            for(int j=2; j<=9; j++) {
+                System.out.printf("%d * %d = %2d", j, i, (j*i));
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+ 
+}
